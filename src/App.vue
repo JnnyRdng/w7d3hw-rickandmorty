7bce46805ef76401c491755a235eee71ac43667f
marekboro <marek.borowicz83@gmail.com>
2020-08-26 17:09:32 +0100

    populated  app.vue and added eventBus to main.js

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,33 @@
-<template>
-  
-</template>
+<template></template>
 
 <script>
-export default {
+import { eventBus } from "./main.js";
+import CharacterList from "./components/CharacterList.vue";
+import CharacterDetail from "./components/CharacterDetail.vue";
 
-}
+export default {
+  name: "App",
+  data() {
+    return {
+      characters: [],
+      selectedCharacter: null,
+    };
+  },
+  mounted() {
+    fetch("https://rickandmortyapi.com/api/character/")
+      .then((response) => response.json())
+      .then((characters) => (this.characters = characters));
+
+    eventBus.$on("character-selected", (character) => {
+      this.selectedCharacter = character;
+    });
+  },
+  components: {
+    "character-list": CharacterList,
+    "character-detail": CharacterDetail,
+  },
+};
 </script>
 
 <style>
-
 </style>
